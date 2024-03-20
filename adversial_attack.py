182157600f7311e63082d5fde59dae6571af3144
Jonas Jakubassa <jakubassa@posteo.de>
2024-03-20 18:21:43 +0100

    intergrate ruben's changes

--- adversial_attack.py
+++ adversial_attack.py
@@ -6,9 +6,40 @@ from mmengine.config import Config
 from mmengine import ConfigDict
 from mmengine.runner import Runner
 
-def apply_adversarial_attack(image, model):
-    perturbed_image = image.clone()  
+# TODO: Is from Cospgd Github  -> how to change?
+def fgsm_attack(self, perturbed_image, data_grad, orig_image):
+    # Collect the element-wise sign of the data gradient
+    sign_data_grad = data_grad.sign()
+    # Create the perturbed image by adjusting each pixel of the input image        
+    if self.targeted:
+        sign_data_grad *= -1
+    perturbed_image = perturbed_image.detach() + self.alpha*sign_data_grad
+    # Adding clipping to maintain [0,1] range
+    if self.norm == 'inf':
+        delta = torch.clamp(perturbed_image - orig_image, min = -1*self.epsilon, max=self.epsilon)
+    elif self.norm == 'two':
+        delta = perturbed_image - orig_image
+        delta_norms = torch.norm(delta.view(self.batch_size, -1), p=2, dim=1)
+        factor = self.epsilon / delta_norms
+        factor = torch.min(factor, torch.ones_like(delta_norms))
+        delta = delta * factor.view(-1, 1, 1, 1)
+    perturbed_image = torch.clamp(orig_image + delta, 0, 1)
+    # Return the perturbed image
+    return perturbed_image
+
+def apply_adversarial_attack(image, model, attack_type="fgsm"):
+    
     #TODO: do a proper attack
+    if attack_type == "fgsm":
+        perturbed_image = image.clone().detach().to("cuda")
+        perturbed_image.requires_grad = True
+        # TODO: look in evaluation file how do they calculate loss?
+    elif attack_type == "pgd":
+        raise NotImplementedError(f"Attack type {attack_type} not implemented yet")
+    elif attack_type == "cospgd":
+        raise NotImplementedError(f"Attack type {attack_type} not implemented yet")
+    else:
+        raise NotImplementedError(f"Attack type {attack_type} not implemented")
     return perturbed_image
 
 # Setup
